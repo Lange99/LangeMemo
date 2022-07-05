12b1f092da9a1dccaf505e89a84ea5757eb665ce
Lange99 <c.langellotti@studenti.unibs.it>
2022-07-05 22:00:55 +0200

    Init commit.

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use teloxide::{prelude::*, utils::command::BotCommands};
+use std::error::Error;
+
+#[tokio::main]
+async fn main() {
+    let bot = Bot::from_env().auto_send();
+    teloxide::commands_repl(bot, answer, Command::ty()).await;
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename = "lowercase", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "Starts the bot.")]
+    Start
+    
+}
+
+
+async fn answer(
+    bot: AutoSend<Bot>,
+    message: Message,
+    command: Command,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    Ok(())
+}
